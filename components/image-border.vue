d9bfdc240f6e03b81d29e7775c0d453082417949
Lc <641229781@qq.com>
2020-09-18 16:08:15 +0800

    父向子传值,子向父传值

---
+++ components/image-border.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<image :src="src" mode="widthFix" @tap="handleImageClick(src)"></image>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		props:{
+			src:String
+		},
+		methods:{
+			handleImageClick(src) {
+				this.$emit("handleImageClick",src)
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/index/index5.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="content">
+		<view >图片地址: {{ srcImageUrl }}</view>
+		<image-border :src="src1" @handleImageClick="handleImageClick"></image-border>
+		<image-border :src="src2" @handleImageClick="handleImageClick"></image-border>
+	</view>
+</template>
+
+<script>
+	import imageBorder from '@/components/image-border.vue'
+	export default {
+		components: {
+			imageBorder
+		},
+		data() {
+			return{
+				srcImageUrl: "",
+				src1:"https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2093143442,3189902132&fm=26&gp=0.jpg",
+				src2:"https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3380212121,1825347810&fm=111&gp=0.jpg"
+			}
+		},
+		methods:{
+			handleImageClick(src) {
+				this.srcImageUrl = src
+			}
+		},
+		onLoad() {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
